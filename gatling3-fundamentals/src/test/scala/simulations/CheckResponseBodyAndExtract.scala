0cc48ef9dc59c6b0a8b41c456f9683c574e6e57a
Gerd Reiss <gerd@reiss.pro>
2020-12-08 16:12:22 +0100

    Gatling Fundamentals: check json path

--- gatling3-fundamentals/src/test/scala/simulations/CheckResponseBodyAndExtract.scala
+++ gatling3-fundamentals/src/test/scala/simulations/CheckResponseBodyAndExtract.scala
@@ -8,12 +8,19 @@ class CheckResponseBodyAndExtract extends Simulation {
   val httpConf = http.baseUrl("http://localhost:8080/app/")
     .header("Accept", "application/json")
 
-
   val scn = scenario("check JSON Path")
 
-    .exec(http("get specific game")
+    .exec(http("get specific video game")
       .get("videogames/1")
       .check(jsonPath("$.name").is("Resident Evil 4")))
+
+    .exec(http("get all video games")
+      .get("videogames")
+      .check(jsonPath(s"${1}.id").saveAs("gameId")))
+
+    .exec(http("get specific video game")
+      .get("videogames/${gameId}")
+      .check(jsonPath("$.name").is("Gran Turismo 3")))
 
   setUp(
     scn.inject(atOnceUsers(1))
