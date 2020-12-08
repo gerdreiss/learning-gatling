b9492d81ff8d612a14bdcfdc1b747f543df50e25
Gerd Reiss <gerd@reiss.pro>
2020-12-08 15:40:27 +0100

    Gatling Fundamentals: first scripts

---
+++ gatling3-fundamentals/src/test/scala/FirstTest.scala
@@ -0,0 +1,18 @@
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class FirstTest extends Simulation {
+
+  // 1. Http Configuration
+  val httpConf = http.baseUrl("http://localhost:8080")
+    .header("Accept", "application/json")
+    .proxy(Proxy("localhost", 8888))
+
+  // 2. Scenario Definition
+  val scn = scenario("The First Test")
+    .exec(http("Get All Games").get("/videogames"))
+
+  // 3. Load Scenario
+  setUp(scn.inject(atOnceUsers(1)))
+    .protocols(httpConf)
+}
