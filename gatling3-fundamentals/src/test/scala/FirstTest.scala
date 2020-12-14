e448aa09a2af300382aad39010ea7671ed58c3ac
Gerd Reiss <gerd@reiss.pro>
2020-12-14 13:08:19 +0100

    Gatling Fundamentals: run simulation from cli

--- gatling3-fundamentals/src/test/scala/FirstTest.scala
+++ gatling3-fundamentals/src/test/scala/FirstTest.scala
@@ -4,13 +4,13 @@ import io.gatling.http.Predef._
 class FirstTest extends Simulation {
 
   // 1. Http Configuration
-  val httpConf = http.baseUrl("http://localhost:8080")
+  val httpConf = http.baseUrl("http://localhost:8080/app/")
     .header("Accept", "application/json")
     .proxy(Proxy("localhost", 8888))
 
   // 2. Scenario Definition
   val scn = scenario("The First Test")
-    .exec(http("Get All Games").get("/videogames"))
+    .exec(http("Get All Games").get("videogames"))
 
   // 3. Load Scenario
   setUp(scn.inject(atOnceUsers(1)))
---
+++ gatling3-fundamentals/src/test/scala/simulations/RuntimeParameters.scala
@@ -0,0 +1,61 @@
+package simulations
+
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ChainBuilder
+import io.gatling.http.Predef._
+
+import scala.concurrent.duration.{DurationInt, FiniteDuration}
+
+class RuntimeParameters extends Simulation {
+
+  before {
+    println(s"Running test with ${userCount} users")
+    println(s"Ramping users over ${rampDuration} seconds")
+    println(s"Total test duration: ${testDuration} seconds")
+  }
+
+
+  // 1. Http Configuration
+  val httpConf = http.baseUrl("http://localhost:8080/app/")
+    .header("Accept", "application/json")
+
+  // 2. Scenario Definition
+  val scn = scenario("Get all video games")
+    .forever() {
+      exec(getAllVideoGames())
+    }
+
+  // 3. Load Scenario
+  setUp(scn
+    .inject(
+      nothingFor(5 seconds),
+      rampUsers(userCount).during(rampDuration)))
+    .protocols(httpConf)
+    .maxDuration(testDuration)
+
+  //
+  // helper functions
+  //
+
+  def getAllVideoGames(): ChainBuilder =
+    exec(http("Get all video games")
+      .get("videogames")
+      .check(status.is(200)))
+
+  def getSpecificVideoGame(id: Int): ChainBuilder =
+    exec(http("Get specific video game")
+      .get(s"videogames/$id")
+      .check(status.is(200)))
+
+  def userCount: Int = getProperty("USERS", "5").toInt
+  def rampDuration: FiniteDuration = getProperty("RAMP_DURATION", "10").toInt.seconds
+  def testDuration: FiniteDuration = getProperty("TEST_DURATION", "60").toInt.seconds
+
+  private def getProperty(propertyName: String, defaultValue: String): String = {
+    Option(System.getenv(propertyName))
+      .orElse(Option(System.getProperty(propertyName)))
+      .getOrElse(defaultValue)
+  }
+
+
+}
